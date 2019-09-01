77ac9d4c4d52e1bb44e2c93740736bc4328f7d57
kurokoji <shu.kakihana@gmail.com>
2019-09-01 19:22:47 +0900

    feat: types.hppを定義

---
+++ solver/include/types.hpp
@@ -0,0 +1,17 @@
+#ifndef INCLUDE_TYPE_HPP
+#define INCLUDE_TYPE_HPP
+
+#include <cstdint>
+
+namespace procon30 {
+  using int8 = std::int_fast8_t;
+  using int16 = std::int_fast16_t;
+  using int32 = std::int_fast32_t;
+  using int64 = std::int_fast64_t;
+  using uint8 = std::uint_fast8_t;
+  using uint16 = std::uint_fast16_t;
+  using uint32 = std::uint_fast32_t;
+  using uint64 = std::uint_fast64_t;
+} // namespace procon30
+
+#endif
